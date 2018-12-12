0d9ab5d9164014612a1eb0304bf8c0357220a821
Joshua Plicque <plicjo@gmail.com>
2018-12-11 20:23:09 -0500

    Specify port for npm start

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+var port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('A basic node app running on Heroku!'))
-
+app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
