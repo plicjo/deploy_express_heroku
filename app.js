b90f21323a854c6b6ea72703be20e0cfa1bb3c36
Joshua Plicque <plicjo@gmail.com>
2018-12-11 20:29:03 -0500

    Change message

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 var port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World from Heroku!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
